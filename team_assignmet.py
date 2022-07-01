1defe1ac57eaab4e3a5598c1de2903177c563a1d
Hyeji Shin <s.hyeji20@gmail.com>
2022-07-01 16:15:15 +0900

    merged with main

--- team_assignmet.py
+++ team_assignmet.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-students = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6']
+students = ['신혜지']
 
-teams = [['name1', 'name4']]
+teams_fixed = [['name1'], ['name3']]
+teams_fixed_backup = list(teams_fixed)
 
 # shuffle students with a specific random seed
 shuffle_idx = np.random.permutation(len(students))
 print(shuffle_idx)
 
 # number of students/team
-n = 2
+n = 3
 
 # team_assignment
 id = 0
@@ -19,12 +20,15 @@ to_break = False
 while True:
     team = []
     members = 0
+    if len(teams_fixed)>0:
+        team = teams_fixed.pop()
+        members= len(team)
     while members<n:
         shuffle_id = shuffle_idx[id]
         name = students[shuffle_id]
         #check if this name already included in other teams
         exist = False
-        for t in teams:
+        for t in teams_fixed_backup:
             if name in t:
                 exist = True
                 break
@@ -43,34 +47,5 @@ while True:
         
 for t in teams:
     print(t)
-=======
-while True:
-    team = []
-    members = 0
-    while members<n:
-        shuffle_id = shuffle_idx[id]
-        name = students[shuffle_id]
-        # check if this name already included in other teams
-        exist = False
-        for t in teams:
-            if name in t:
-                exist = True
-                break
-        
-        if not exist: 
-            team.append(name)
-            members += 1
-        id = id+1
-        if id >= len(shuffle_idx):
-            to_break = True
-            break
-    
-    teams.append(team)
-    if to_break:
-        break
-
-for t in teams:
-    print(t)
-        
  
 >>>>>>> origin/main
